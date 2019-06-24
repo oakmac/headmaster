dd39acadd0769704c68b9fad3c85bff38f1c4f11
Amanda Shih <pandafulmanda@gmail.com>
2019-06-24 04:32:32 -0500

    reduce some copying and pasting; making model guts with json parsing helper

--- src-backend/app/models/github-response.js
+++ src-backend/app/models/github-response.js
@@ -4,7 +4,7 @@ const {
 } = require('date-fns')
 const R = require('ramda')
 
-const createGuts = require('../helpers/model-guts')
+const createGuts = require('../helpers/model-guts-with-json')
 
 const name = 'StudentGithubResponse'
 const tableName = 'StudentsGithubResponses'
@@ -24,58 +24,19 @@ module.exports = knex => {
     selectableProps
   })
 
-  // TODO need to just make this common guts.
-  const stringifyForSQLite = R.evolve({
-    body: JSON.stringify,
-  })
-
-  const parseFromSQLite = R.evolve({
-    body: JSON.parse,
-  })
-
-  function create(props) {
-    return guts.create(
-      stringifyForSQLite(props)
-    )
-  }
-
-  function bulkCreate(array) {
-    return guts
-      .bulkCreate(R.map(
-        stringifyForSQLite
-      )(array))
-  }
-
-  function update(props) {
-    return guts.update(
-      stringifyForSQLite(props)
-    )
-  }
-
-  function find(filters) {
-    return guts.find(filters)
-      .then(function(results) {
-        return R.map(
-          parseFromSQLite
-        )(results)
-      })
-  }
-
   function getLatestResponse() {
     return knex.from(tableName)
       .groupBy('studentId')
       .orderBy('createdAt', 'desc')
       .whereNotNull('body')
+      .select(knex.raw(`max(${tableName}.createdAt) as createdAt, studentId, body`))
   } 
 
   // TODO make more flexible in time units since
   function getStaleStudents(classId, daysSince = 1) {
     return  knex.from('Students')
       .leftJoin(
-        getLatestResponse()
-          .select(knex.raw(`max(${tableName}.createdAt) as createdAt, studentId, body`))
-          .as('latest')
-        ,
+        getLatestResponse().as('latest'),
         'Students.id', `latest.studentId`
       )
       .where('Students.classId', classId)
@@ -92,13 +53,6 @@ module.exports = knex => {
 
   return {
     ...guts,
-    create,
-    update,
-    find,
-    bulkCreate,
-    // could export outside of init, but TODO for later.
-    stringifyForSQLite,
-    parseFromSQLite,
     getStaleStudents,
   }
 }

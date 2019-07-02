7181c5b31fbf63c012aeb4dec82b09e84a4b12e0
patrick.hildreth <patrick.hildreth@patrick-hildreth-mbp.local>
2019-07-02 13:14:27 -0500

    create invite-student ajax func

--- src-cljs/headmaster/ui/ajax.cljs
+++ src-cljs/headmaster/ui/ajax.cljs
@@ -17,6 +17,12 @@
      :keywords? true
      :response-format :json}))
 
+(defn invite-student [class-id student-id success-fn error-fn]
+  (if (in-mock-mode?)
+    (js/setTimeout success-fn (+ 250 (rand-int 500)))
+    (POST (str "api/classroom" class-id "invitation")))
+  )
+
 (defn create-touchpoint [student-id touchpoint success-fn error-fn]
   (if (in-mock-mode?)
     (js/setTimeout success-fn (+ 250 (rand-int 500)))

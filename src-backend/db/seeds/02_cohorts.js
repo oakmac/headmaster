8d5fe38b0bb727acb9bfd317db87673c07876623
Chris Oakman <chris@oakmac.com>
2019-07-17 09:58:19 -0500

    minor - "class" --> "cohort" in the seed data

--- src-backend/db/seeds/02_cohorts.js
+++ src-backend/db/seeds/02_cohorts.js
@@ -6,8 +6,8 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('Classes').insert([
-        {id: 'Class-8948c7b6-18da-4351-b457-16887d9d372e', slug: 'demo-class-1', name: 'Demo Class 1'},
-        {id: 'Class-5d409547-df86-4020-84a7-8ac5a7925fb2', slug: 'demo-class-2', name: 'Demo Class 2'}
+        {id: 'Class-8948c7b6-18da-4351-b457-16887d9d372e', slug: 'demo-cohort-1', name: 'Demo Cohort 1'},
+        {id: 'Class-5d409547-df86-4020-84a7-8ac5a7925fb2', slug: 'demo-cohort-2', name: 'Demo Cohort 2'}
       ]);
     });
 };
